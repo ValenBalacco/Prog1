40957227cefe9ba23c856a7fb12aacf80d83d87e
ValenBalacco <valentino.balacco.16@gmail.com>
2023-08-15 18:49:23 -0300

    Create Ej Complementarios.py

---
+++ Ej Complementarios.py
@@ -0,0 +1,53 @@
+#Punto 1
+numero1 = 16
+#Punto 2
+numero2 = 3.17
+#Punto 3
+suma = numero1 + numero2
+print(suma)
+#Punto 4
+resta = numero1 - numero2
+print(resta)
+multiplicacion = numero1 * numero2
+print(multiplicacion)
+division = numero1 / numero2
+print(division)
+#Punto 5
+nombre = 'Valen'
+#Punto 6
+precio = 3999
+#Punto 7
+descuento = 0.25
+#Punto 8
+precio_final = precio-precio*descuento
+print(precio_final)
+#Punto 9
+cadena = "Hola me llamo"
+#Punto 10
+longitud = len(cadena)
+print(longitud)
+#Punto 11
+precio = 23.75
+print(int(precio))
+#Punto 12
+nombre = 'Valen'
+apellido = ' Balacco'
+nombre_completo = nombre + apellido
+print(nombre_completo)
+#Punto 13
+edad = 18
+edad = edad+5
+print(edad)
+print(edad-10)
+#Punto 14
+altura = 1.72
+altura = altura*4
+print(altura)
+altura = altura/3
+print(altura)
+#Punto 15
+nombre2 = "Valen Balacco"
+print(nombre2.upper())
+print(nombre2.lower())
+#Punto 16
+print(nombre2.title())
